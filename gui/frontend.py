03ca7c23f222a4abcfe613b21d929eb9960a56cd
Mohammed <saleem@simplylion.com>
2023-11-16 12:39:15 -0500

    Added pages for genres and playlists

--- gui/frontend.py
+++ gui/frontend.py
@@ -1,7 +1,30 @@
 from flask import Flask, render_template
+import sys, os
+
+# get the current directory
+current_dir = os.path.dirname(os.path.realpath(__file__))
+
+# get the parent directory
+parent_dir = os.path.dirname(current_dir)
+
+# import main.py from the parent directory
+sys.path.append(parent_dir)
+
+from music.classes import Song, Playlist, Recommender
 
 app = Flask(__name__)
 
+# Homepage
 @app.route("/")
-def hello_world():
-    return render_template("index.html")
+def index():
+    return render_template("index.html", title="Home")
+
+# Playlist Page
+@app.route("/playlists")
+def playlists_page():
+    return render_template("playlists.html", title="Playlists")
+
+# Genres Page
+@app.route("/genres")
+def genres_page():
+    return render_template("genres.html", title="Genres")
