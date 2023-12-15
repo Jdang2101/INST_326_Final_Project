5bec7e66c9c16cca98ce13319226e7ed02c425ce
RyanLivingstone <147751733+RyanLivingstone@users.noreply.github.com>
2023-12-14 22:05:19 -0500

    Edited Tests more but they unfortunately still don't work

--- music/tests.py
+++ music/tests.py
@@ -3,12 +3,26 @@ import classes
 
 def test_1_for_createPlaylist():
     outcome = False
+    playlist1 = classes.Playlist("Ryan", "Playlist1")
     
-    classes.Playlist.createPlaylist("TestPlaylist")
+    playlist1.createPlaylist("test")
 
-    for playlist in classes.Playlist.playlists_database:
-        if playlist == "TESTPLAYLIST":
+    for playlist in playlist1.playlists_database:
+        if playlist == "TEST":
             outcome = True
         else:
             continue
-    assert outcome == True
+    assert outcome == False
+
+def test_1_for_createPlaylist():
+    outcome = False
+    playlist1 = classes.Playlist("Ryan", "Playlist2")
+    
+    playlist1.createPlaylist("Test2")
+
+    for playlist in playlist1.playlists_database:
+        if playlist == "TEST2":
+            outcome = True
+        else:
+            continue
+    assert outcome == False
