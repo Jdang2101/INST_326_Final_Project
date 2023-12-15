332c2b20bd1aa55d156b96114c32d8deb8d69ddd
Mohammed <saleem@simplylion.com>
2023-12-15 08:42:21 -0500

    added tests for the GUI

--- tests.py
+++ tests.py
@@ -1,9 +1,11 @@
 from music.classes import Playlist
 import unittest
+from gui.frontend import app
 
 class TestPlaylist(unittest.TestCase):
     """
-    Test the Playlist class"""
+    Test the Playlist class by creating a playlist and checking if it is in the database
+    """
     
     def test_1_for_createPlaylist(self):
         playlist1 = Playlist("Ryan", "Playlist1")
@@ -26,6 +28,25 @@ class TestPlaylist(unittest.TestCase):
                 self.assertEqual(playlist.playlist_title, "TEST2")
             else:
                 continue
+
+class TestFrontend(unittest.TestCase):
+    """
+    Test the frontend of the application
+    """
+    def test_1_for_index(self):
+        tester = app.test_client(self)
+        response = tester.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_2_for_playlists(self):
+        tester = app.test_client(self)
+        response = tester.get("/playlists")
+        self.assertEqual(response.status_code, 200)
+
+    def test_3_for_playlist_create(self):
+        tester = app.test_client(self)
+        response = tester.get("/playlists/create")
+        self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
